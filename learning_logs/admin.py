8b943bc8ab64c49a67e6785369af5d67fe07e8af
ndunguP <peter.ndunguoz50@gmail.com>
2024-08-15 01:53:09 -0700

    adminpage

--- learning_logs/admin.py
+++ learning_logs/admin.py
@@ -2,5 +2,7 @@ from django.contrib import admin
 
 # Register your models here.
 from django.contrib import admin
+from .models import Topic, Entry
 from .models import Topic
 admin.site.register(Topic)
+admin.site.register(Entry)
