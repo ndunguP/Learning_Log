0d24535fa665e563200daf1815f9f2e18dedffdc
ndunguP <peter.ndunguoz50@gmail.com>
2024-08-17 06:12:19 -0700

    functional_all

--- ll_project/urls.py
+++ ll_project/urls.py
@@ -16,9 +16,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
-from django.urls import path
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('accounts/', include('accounts.urls')),
     path('', include('learning_logs.urls')),
 ]
